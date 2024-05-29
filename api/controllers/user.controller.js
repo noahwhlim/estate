50876abb64de9592f1907b95ac5eb825b17a4a1b
noahwhlim <noahhlimwh@gmail.com>
2024-05-28 22:52:36 -0500

    added user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,37 @@
+import bcrypt from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import User from '../models/user.model.js';
+
 export const test = (req, res) => {
     res.json({
         message: "API Route is working"
     })
 }
 
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, "You can only update your own account!"));
+    }
+
+    try {
+        if (req.body.password) {
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: { // $set is a MongoDB operator that allows you to update the fields of a document
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            },
+        }, { new: true });
+
+        const { password, ...rest } = updatedUser._doc; // Remove password from the response
+
+        res.status(200).json(rest)
+
+    } catch (err) {
+        next(err)
+    }
+}
