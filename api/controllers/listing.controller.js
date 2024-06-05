d7ffb3a3de0d521c2ae2b48cad6cd03e6cb488ab
noahwhlim <noahhlimwh@gmail.com>
2024-06-04 19:30:22 -0500

    create search function api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -37,12 +37,16 @@ export const updateListing = async (req, res, next) => {
   }
 
   try {
-    const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
     res.status(200).json(updatedListing);
   } catch (err) {
     next(err);
   }
-}
+};
 
 export const getListing = async (req, res, next) => {
   try {
@@ -54,4 +58,50 @@ export const getListing = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] }; // mongodb query: select all offers that have false or true value
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" }, // $regex: search for a string, $options: case insensitive
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (err) {
+    next(err);
+  }
+};
